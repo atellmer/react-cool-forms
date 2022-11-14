e02afbe246c9071aefe443cd90182cfdc0f3fb8f
atellmer <atellmer@gmail.com>
2022-11-14 00:47:49 -0800

    build

--- examples/table-form/webpack.config.js
+++ examples/table-form/webpack.config.js
@@ -27,7 +27,7 @@ const config = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
   ],
 };
